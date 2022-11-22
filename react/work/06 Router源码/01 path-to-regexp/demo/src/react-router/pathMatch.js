f978fefa44cbe908eafd57f4b0c63a2fc7f4753d
dengzixu <sk8sigua@aliyun.com>
2022-11-22 10:13:32 +0800

    2022.8.02/15.46-luffa_dzx

--- react/work/06 Router源码/01 path-to-regexp/demo/src/react-router/pathMatch.js
+++ react/work/06 Router源码/01 path-to-regexp/demo/src/react-router/pathMatch.js
@@ -1,3 +1,4 @@
+
 import { pathToRegexp } from "path-to-regexp"
 
 /// 得到匹配结果  匹配结果是一个对象 该对象的属性名对应路径规则中的关键字 ，如果不能匹配返回undefined 
@@ -6,6 +7,24 @@ const pathMatch = (path, url, options) => {
     let keys = [];//保存路径规则中的关键字
     const regEXp = pathToRegexp(path, keys, options);
     const result = regEXp.exec(url)
-    console.log(result);
+    if (!result) {
+        return;
+    }
+    let execArr = Array.from(result)
+    execArr = execArr.slice(1)
+    console.log(execArr, url);
+   const params= getparams(result, url)
+   console.log(params);
 };
+
+function getparams(gruops, url) {
+    const obj = {}
+    for (let index = 0; index < gruops.length; index++) {
+        const value = gruops[index]
+        const name = url[index].name;
+        obj[name] = value
+    }
+    return obj
+}
+pathMatch("/news/:id/:page", "/news/234/1")
 export default pathMatch;
