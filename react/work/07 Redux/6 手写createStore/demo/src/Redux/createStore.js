15c85a67fcaaf27f67eb4fb7bfba135fe91488d9
dengzixu <sk8sigua@aliyun.com>
2022-11-27 18:05:42 +0800

    2022.8.02/15.46-luffa_dzx

--- react/work/07 Redux/6 手写createStore/demo/src/Redux/createStore.js
+++ react/work/07 Redux/6 手写createStore/demo/src/Redux/createStore.js
@@ -18,6 +18,8 @@ export default function (reducer, defaultState) {
 
     let currentreducer = reducer;//当前使用的reducer
     let currentdefaultState = defaultState;//当前仓库中的状态
+
+    const listeners=[]//记录所有的监听器
     function dispatch(action) {
         if (isPlainObject(action)) {
             throw new TypeError("action must be a plain object ")
@@ -27,12 +29,28 @@ export default function (reducer, defaultState) {
             throw new TypeError("action must has property of type")
         }
         currentdefaultState = currentreducer(currentdefaultState, action)
+        
+        //运行所有的订阅者（监听器）
+        for(const listener of listeners){
+            listener()
+        }
     }
     function getState() {
         return currentdefaultState
     }
+    //添加一个监听器(订阅者)
     function subscribe(listener) {
-
+       listeners.push(listener)//将监听器加入到数组中
+       let isRemove=false//是否已经移除
+       return function(){
+           if(isRemove){
+               return
+           }
+        //  将listener从数组中移除
+           const index = listeners.indexOf(listener)
+           listeners.splice(index,1)
+           isRemove=true
+      }
     }
     //创建仓库时，需要分发一次初始的action
     dispatch({
