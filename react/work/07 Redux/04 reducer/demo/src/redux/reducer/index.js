9ff153fcfc3e1bab6cd8029e3993591be029d3a0
dengzixu <sk8sigua@aliyun.com>
2022-11-29 14:16:20 +0800

    2022.8.02/15.46-luffa_dzx

--- react/work/07 Redux/04 reducer/demo/src/redux/reducer/index.js
+++ react/work/07 Redux/04 reducer/demo/src/redux/reducer/index.js
@@ -1,25 +1,19 @@
-import * as ActipnType from "../action/actionType"
-function reducer(state=10, action) {
-    console.log("reducer运行了",state);
-    switch(action.type){
-        case ActipnType.Increase:
-            return state + 1
-        case ActipnType.Decrease:
-            return state - 1
-        case ActipnType.Set:
-            return action.payload
-            default: 
-                return
-    }
-    // 返回一个新的状态
-    // if (action.type === ActipnType.Increase) {
-    //     return state + 1
-    // } else if (action.type === ActipnType.Decrease) {
-    //     return state - 1
-    // } else if (action.type === ActipnType.Set) {
-    //     return action.payload
-    // }
-    // return state;//如果type是无效的，数据不变 
+import Loginusers from "./loginUser";
+import users from "./users";
 
-}
-export default reducer
+ const initialState = {
+     Loginusers:null,
+     users:null,
+ }
+ 
+ export default (state = initialState, { type, payload }) => {
+   switch (type) {
+ 
+   case first:
+     return { ...state, ...payload }
+ 
+   default:
+     return state
+   }
+ }
+ 
