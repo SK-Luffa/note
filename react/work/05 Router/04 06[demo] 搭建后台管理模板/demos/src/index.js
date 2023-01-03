a1fb86dbcdf9582acf5c2ba5a4a500bf7b36da30
dengzixu <sk8sigua@aliyun.com>
2023-01-03 17:30:19 +0800

    2022.8.02/15.46-luffa_dzx

--- react/work/05 Router/04 06[demo] 搭建后台管理模板/demos/src/index.js
+++ react/work/05 Router/04 06[demo] 搭建后台管理模板/demos/src/index.js
@@ -4,6 +4,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <App />
+    <App  />
 );
 
---
+++ react/work/08 Redux中间件/02 Redux-thunk/react-learn/README.md
@@ -0,0 +1,5 @@
+# 利用中间件进行副作用处理
+
+- redux-thunk
+- redux-promise
+- redux-saga
