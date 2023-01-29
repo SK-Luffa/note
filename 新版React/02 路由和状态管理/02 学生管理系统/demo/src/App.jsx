6d80466966b92c4bd1dfd8016a7cdd0c13513902
dengzixu <sk8sigua@aliyun.com>
2023-01-30 06:48:19 +0800

    2022.8.02/15.46-luffa_dzx

---
+++ 新版React/02 路由和状态管理/02 学生管理系统/demo/src/App.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+import {Route,Routes} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Add from './components/Add'
+import "./CSS/App.css"
+
+export default function App() {
+  return (
+    // 最外层容器
+    <div className="container" id='app'>
+      {/* 导航栏 */}
+      <nav className="navbar navbar-inverse   navbar-fixed-top">
+        <div className="container">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+            <a className="navbar-brand" href="#">学生管理系统</a>
+          </div>
+          <div id="navbar" className="collapse navbar-collapse">
+            <ul className="nav navbar-nav">
+              <li className="active"><a href="#">主页</a></li>
+              <li><a href="#">关于我们</a></li>
+
+            </ul>
+            <ul className="nav navbar-nav navbar-right">
+              <li ><a href="#">添加用户 </a></li>
+            </ul>
+          </div>
+        </div> 
+      </nav>
+      <div className="routers">
+        
+        {/* 匹配上的路由所对应的组件显示在这个位置 */}
+        <Routes >
+          {/* 在route组件中书写对应的路由，以及路由所对应的组件 */}
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/add' element={<Add />} />
+        </Routes> 
+      </div>
+     
+    </div>
+   
+  )
+}
