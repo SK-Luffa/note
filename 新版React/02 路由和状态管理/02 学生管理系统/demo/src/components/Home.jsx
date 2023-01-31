07cbb018ae7b3258e549c2e7bb749d462e3a6c3c
dengzixu <sk8sigua@aliyun.com>
2023-01-31 08:23:46 +0800

    2022.8.02/15.46-luffa_dzx

--- 新版React/02 路由和状态管理/02 学生管理系统/demo/src/components/Home.jsx
+++ 新版React/02 路由和状态管理/02 学生管理系统/demo/src/components/Home.jsx
@@ -1,19 +1,54 @@
- import {React,useEffect} from 'react'
- import {getStuListApi} from "../api/stuApi"
+import { React, useEffect, useState } from 'react'
+import { getStuListApi } from "../api/stuApi"
 
 
- export default function Home() {
+export default function Home() {
+    const [stuList, setStuList] = useState([])
+    const [serachItem, setSearchItem] = useState([])
+    //注意这里需要添加依赖项为空数组，代表只执行一次
+    useEffect(() => {
+        getStuListApi().then(({ data }) => {
 
-    useEffect(()=>{
-        getStuListApi().then((res)=>{
-
-            console.log(res);
+            setStuList(data);
+            console.log(data);
+            // console.log(setStuList);
         })
+    }, [])
+
+    const changeHandle = () => { }
+
+ const trs= stuList.map((item, index) => {
+        return (
+            <tr key={index}>
+                <td>{item.name}</td>
+                <td>{item.age}</td>
+                <td>{item.phone}</td>
+                <td>详情</td>
+            </tr>
+        )
     })
 
+    return (
+        <div>
+            <h1>学生列表</h1>
+            {/* 搜索 */}
+            <input type="text" className='form-control' placeholder='搜索' value={serachItem} onChange={changeHandle} />
 
-   return (
-     <div>Home</div>
-   )
- }
- 
+            {/* 表格 */}
+
+            <table className='table table-striped table-bordered'>
+                <thead>
+                    <tr>
+                        <th>姓名</th>
+                        <th>年龄</th>
+                        <th>联系方式</th>
+                        <th>操作 </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {trs}
+                </tbody>
+            </table>
+        </div>
+    )
+}
